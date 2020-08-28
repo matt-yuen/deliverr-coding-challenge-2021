d02b6122c77b7129388c034d872a61e4e421959c
Matthew Yuen <un.matthew@gmail.com>
2020-08-27 22:06:59 -0400

    Plan out test case

---
+++ src/test/inventory-allocator.test.js
@@ -0,0 +1,13 @@
+const expect = require('chai').expect;
+
+describe('InventoryAllocator', () => {
+  describe('findCheapestShipment', () => {
+    it('should return an empty array if given no orders', () => {})
+    it('should return an empty array if given no warehouses', () => {})
+    it('should return an empty array if the warehouses do not contain any order items', () => {})
+    it('should return an empty array if the warehouses do not have enough inventory', () => {})
+    it('should return a shipment from a warehouse with inventory matching the ordered items', () => {})
+    it('should return shipments across multiple warehouses', () => {})
+    it('should return shipments if given multiple orders', () => {})
+  })
+})
